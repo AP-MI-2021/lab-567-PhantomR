da78f877d859d56c89cc4c7ca2c44e95fc8c0927
Alexandru Dan Duna <dunaalexdan@yahoo.com>
2021-10-27 19:23:50 +0300

    Add stub functions in console.py and implement main.py

---
+++ Logic/booking_crud.py
@@ -0,0 +1,1 @@
+from Domain.booking import *
---
+++ UserInterface/console.py
@@ -0,0 +1,33 @@
+def ui_show_main_menu():
+    print('''
+    MAIN MENU
+    What would you like to do?
+    -------------------------------------------
+    1. Create / Read / Update / Delete Bookings
+    2. Other operations on Bookings
+    3. Undo / Redo
+    -------------------------------------------
+    0. Exit
+    ''')
+
+
+def ui_show_crud_menu():
+    print('''
+    CRUD MENU
+    What would you like to do?
+    -------------------------------------------
+    1. Create a new Booking
+    2. Display all Bookings
+    3. Edit a Booking
+    4. Delete a Booking
+    -------------------------------------------
+    0. Go back to the Main Menu
+    ''')
+
+
+def ui_run_main_menu_loop():
+    pass
+
+
+def ui_run_crud_menu_loop():
+    pass
